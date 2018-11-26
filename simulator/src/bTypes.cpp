7f7f2df2e6ab3d3955ad72d681eec1a5833e7cc4
thmouse <45173155+thmouse@users.noreply.github.com>
2018-11-26 12:42:51 +0100

    b typ

--- simulator/src/bTypes.cpp
+++ simulator/src/bTypes.cpp
@@ -3,18 +3,19 @@
 #include "fields.h"
 using namespace std;
 
-uint8_t * beq(line &instr, uint32_t * reg_ptr){
-	uint32_t imm, *r1,*r2;
-	imm = ((instr.instr & EXTRACT_B_IMM_12)>>19); // TODO: check mine hurtige ændringer
-	imm = imm & ((instr.instr & EXTRACT_B_IMM_11)<<4);
-	imm = imm & ((instr.instr & EXTRACT_B_IMM_10_5)>>20);
-	imm = imm & ((instr.instr & EXTRACT_B_IMM_4_1)>>7);
+uint8_t * beq(line &instr, uint32_t * reg_ptr, uint8_t * prgm_counter){
+	uint32_t *r1,*r2;
+	int32_t imm;
+	imm = ((instr.instr & EXTRACT_B_IMM_12)>>19);
+	imm = imm | ((instr.instr & EXTRACT_B_IMM_11)<<4);
+	imm = imm | ((instr.instr & EXTRACT_B_IMM_10_5)>>20);
+	imm = imm | ((instr.instr & EXTRACT_B_IMM_4_1)>>7);
 
 	r1 = reg_ptr+((instr.instr & EXTRACT_RBS_R1)>>15);
 	r2 = reg_ptr+((instr.instr & EXTRACT_RBS_R2)>>20);
-  printf("add x%.2d, x%.2d, x%.2d", ((instr.instr & EXTRACT_R_RD)>>7) , ((instr.instr & EXTRACT_RBS_R1)>>15) , ((instr.instr & EXTRACT_RBS_R2)>>20));
+  printf("beq x%.2d, x%.2d, PC+%d", ((instr.instr & EXTRACT_RBS_R1)>>15) , ((instr.instr & EXTRACT_RBS_R2)>>20) , imm);
 	if(*r1 == *r2){
-    return (uint8_t*)(imm + reg_ptr); //cast til uint8t pointer?
+    return imm + prgm_counter;
   }
-  else return (uint8_t*)reg_ptr;
+  else return prgm_counter+4;
 }
