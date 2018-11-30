3aa112d2c77c70ec603807552d3e396597911cff
thmouse <45173155+thmouse@users.noreply.github.com>
2018-11-30 10:48:16 +0100

    slli srai srli

--- simulator/src/irTypes.cpp
+++ simulator/src/irTypes.cpp
@@ -1,19 +1,37 @@
 using namespace std;
 
 void slli(line &instr, uint32_t * reg_ptr){
-	uint32_t * rd, *r1;
+	uuint32_t *rd, *r1;
+	int32_t imm = 0;
+
+	imm = imm | (((int32_t)instr.instr & EXTRACT_I_IMM_11_0)>>20);
 	rd = reg_ptr+((instr.instr & EXTRACT_R_RD)>>7);
 	r1 = reg_ptr+((instr.instr & EXTRACT_RBS_R1)>>15);
 
-	*rd = *r1<<(*r2);
+	*rd = *r1<<imm;
 	printf("slli x%.2d, x%.2d, x%.2d\n", ((instr.instr & EXTRACT_R_RD)>>7) , ((instr.instr & EXTRACT_RBS_R1)>>15) , ((instr.instr & EXTRACT_RBS_R2)>>20));
 }
 
 void srai(line &instr, uint32_t * reg_ptr){
-	uint32_t * rd, *r1;
+	uuint32_t *rd, *r1;
+	int32_t imm = 0;
+
+	imm = imm | (((int32_t)instr.instr & EXTRACT_I_IMM_11_0)>>20);
 	rd = reg_ptr+((instr.instr & EXTRACT_R_RD)>>7);
 	r1 = reg_ptr+((instr.instr & EXTRACT_RBS_R1)>>15);
-	r2 = reg_ptr+((instr.instr & EXTRACT_RBS_R2)>>20);
-	*rd = (int32_t)*r1>>((int32_t)*r2);
+
+	*rd = (int32_t)(*r1)>>imm;
 	printf("srai x%.2d, x%.2d, x%.2d\n", ((instr.instr & EXTRACT_R_RD)>>7) , ((instr.instr & EXTRACT_RBS_R1)>>15) , ((instr.instr & EXTRACT_RBS_R2)>>20));
+}
+
+void srli(line &instr, uint32_t * reg_ptr){
+	uuint32_t *rd, *r1;
+	int32_t imm = 0;
+
+	imm = imm | (((int32_t)instr.instr & EXTRACT_I_IMM_11_0)>>20);
+	rd = reg_ptr+((instr.instr & EXTRACT_R_RD)>>7);
+	r1 = reg_ptr+((instr.instr & EXTRACT_RBS_R1)>>15);
+
+	*rd = *r1>>imm;
+	printf("srli x%.2d, x%.2d, x%.2d\n", ((instr.instr & EXTRACT_R_RD)>>7) , ((instr.instr & EXTRACT_RBS_R1)>>15) , ((instr.instr & EXTRACT_RBS_R2)>>20));
 }
